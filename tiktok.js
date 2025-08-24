fe2d808fda6699831b355124e019259b6ceacb8b
Garphis <89042127+Garphis@users.noreply.github.com>
2025-08-25 02:48:20 +0300

    Add files via upload

---
+++ tiktok.js
@@ -0,0 +1,19 @@
+let open = false;
+
+function aigit(){
+    
+    window.location.href="Ai.html"
+    
+}
+
+function ttyegit(){
+    
+    window.location.href="index.html"
+    
+}
+
+function garpegit(){
+    
+    window.location.href="garp.html"
+    
+}
